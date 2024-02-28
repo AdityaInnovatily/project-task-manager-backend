4e2662b903041077deeb2cd3f75855e6c82f8bee
AdityaInnovatily <aditya.v@innovatily.com>
2024-02-29 00:41:10 +0530

    authentication added

--- server/tasks/taskController.js
+++ server/tasks/taskController.js
@@ -1,15 +1,55 @@
 const {createTask, updateTask,getTaskById, getTask,
      updateTaskStatus, deleteTask, analytics, updateChecklist} = require("./taskService");
+
+const jwt = require('jsonwebtoken');
 const router = require("express").Router();
 
-router.post("/createTask", createTask);
-router.post("/updateTask", updateTask);
-router.get("/getTaskById/:id", getTaskById);
-router.get("/getTaskList/:userId/:sortBy", getTask);
-router.post("/updateTaskStatus", updateTaskStatus);
-router.delete("/deleteTask/:taskId", deleteTask);
-router.get('/analytics/:userId',analytics);
-router.post('/updateChecklist',updateChecklist);
+
+// const authentication = require("../users/userController");
+
+
+const authentication = (req,res,next)=>{
+
+     let token = req.headers['authorization'];
+   
+     let filter_token = token?.split(" ");
+     console.log('filter_token;',filter_token);
+     // console.log('req.header;',filter_token[1]);
+   
+     if(!token){
+         return res.status(401).json({msg: 'Unauthorized'});
+     }
+   
+     if(filter_token[1] == "undefined"){
+          return res.status(401).json({msg: 'Please, login first!'});
+     }
+
+     jwt.verify(filter_token[1],process.env.SECRET_KEY, (err,user)=>{
+         if(err){
+             return res.status(403).json({msg: 'Token InCorrect !'});
+         }
+   
+         
+         req.user = user;
+        
+     next();
+     console.log("////////////////authentication done///////////////");
+     
+     });
+    
+   }
+
+router.post("/createTask", authentication, createTask);
+router.post("/updateTask", authentication, updateTask);
+router.get("/getTaskById/:id", authentication, getTaskById);
+router.get("/getTaskByIdPublic/:id", getTaskById);
+router.get("/getTaskList/:userId/:sortBy", authentication, getTask);
+router.post("/updateTaskStatus", authentication, updateTaskStatus);
+router.delete("/deleteTask/:taskId", authentication, deleteTask);
+router.get('/analytics/:userId', authentication, analytics);
+router.post('/updateChecklist', authentication, updateChecklist);
+router.post('/updateChecklistPublic', updateChecklist);
+
 
 
 module.exports = router;
--- server/users/userController.js
+++ server/users/userController.js
@@ -1,12 +1,43 @@
-const { registerUser, login, updateUser, analytics } = require("./userService");
+const { registerUser, login, updateUser } = require("./userService");
 const jwt = require('jsonwebtoken');
 const bcrypt = require("bcrypt");
 const router = require("express").Router();
 
 
-router.post('/register', registerUser);
+const authentication = (req,res,next)=>{
+
+    let token = req.headers['authorization'];
+  
+    let filter_token = token?.split(" ");
+    // console.log('req.header;',req.headers);
+    // console.log('req.header;',filter_token[1]);
+  
+    if(!token){
+        return res.status(401).json({msg: 'Unauthorized'});
+    }
+
+    if(filter_token[1] == "undefined"){
+        return res.status(401).json({msg: 'Please, login first!'});
+   }
+  
+    jwt.verify(filter_token[1],process.env.SECRET_KEY, (err,user)=>{
+        if(err){
+            return res.status(403).json({msg: 'Token InCorrect !'});
+        }
+  
+        
+        req.user = user;
+       
+    next();
+    console.log("////////////////authentication done///////////////");
+    
+    });
+   
+  }
+
+router.post('/register',registerUser);
 router.post('/login',login);
-router.post('/updateUser',updateUser);
+router.post('/updateUser', authentication, updateUser);
 
 
 module.exports = router;
